1e4863ad9842ff0b6d9d90e049a00d93495ce47a
MarvinRmx <34686902+MarvinRmx@users.noreply.github.com>
2017-12-19 17:18:16 +0100

    Update server.js
    modifying port listening and adding ping route

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.get('/login', function(req, res){
     res.render('login');
 });
 
-app.listen(1313);
+app.get('/ping', function(req, res){
+    res.sender('pong');
+});
+
+app.listen(process.enc.PORT||1313);
 
 
 
